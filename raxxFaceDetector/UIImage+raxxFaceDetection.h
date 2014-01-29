3b546ea5e755544ed1678dd5b7f8d369f5190e56
Rahim Mitha <rkmitha@gmail.com>
2014-01-29 12:09:22 -0500

    - Updated facial feature detection methods

---
+++ raxxFaceDetector/UIImage+raxxFaceDetection.h
@@ -0,0 +1,13 @@
+//
+//  UIImage+raxxFaceDetection.h
+//  raxxFaceDetector
+//
+//  Created by Rahim Mitha on 1/29/2014.
+//  Copyright (c) 2014 Rahim Mitha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImage (raxxFaceDetection)
+
+@end
---
+++ raxxFaceDetector/UIImage+raxxFaceDetection.m
@@ -0,0 +1,13 @@
+//
+//  UIImage+raxxFaceDetection.m
+//  raxxFaceDetector
+//
+//  Created by Rahim Mitha on 1/29/2014.
+//  Copyright (c) 2014 Rahim Mitha. All rights reserved.
+//
+
+#import "UIImage+raxxFaceDetection.h"
+
+@implementation UIImage (raxxFaceDetection)
+
+@end
