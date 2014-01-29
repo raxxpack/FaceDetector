ec0773f2a5b074d5d8859169564333aaf9b624ba
Rahim Mitha <rahim.mitha@xtremelabs.com>
2014-01-29 10:41:48 -0500

    Initial Commit

---
+++ raxxFaceDetector/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  raxxFaceDetector
+//
+//  Created by Rahim Mitha on 1/29/2014.
+//  Copyright (c) 2014 Rahim Mitha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ raxxFaceDetectorTests/raxxFaceDetectorTests.m
@@ -0,0 +1,34 @@
+//
+//  raxxFaceDetectorTests.m
+//  raxxFaceDetectorTests
+//
+//  Created by Rahim Mitha on 1/29/2014.
+//  Copyright (c) 2014 Rahim Mitha. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface raxxFaceDetectorTests : XCTestCase
+
+@end
+
+@implementation raxxFaceDetectorTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
